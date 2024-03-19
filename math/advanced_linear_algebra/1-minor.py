25eef63e2596d5bc01facd56d2c354615deb09b8
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-03-19 15:48:42 -0500

    create task 1

---
+++ math/advanced_linear_algebra/1-minor.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" calculates the minor matrix of a matrix: """
+determinant = __import__('0-determinant').determinant
+
+
+def minor(matrix):
+    """
+    Calculates the minor matrix of a given matrix.
+
+    Args:
+        matrix (list of lists): The input matrix.
+
+    Returns:
+        list of lists: The minor matrix of the input matrix.
+
+    Raises:
+        TypeError: If matrix is not a list of lists.
+        ValueError: If matrix is not square or is empty.
+    """
+    if not isinstance(
+        matrix, list) or not all(
+            isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    num_rows = len(matrix)
+    if num_rows == 0 or num_rows != len(matrix[0]):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    minor_matrix = []
+    for i in range(num_rows):
+        minor_row = []
+        for j in range(num_rows):
+            minor = [
+                row[:j] + row[j+1:]
+                for row in (matrix[:i] + matrix[i+1:])]
+            minor_row.append(determinant(minor))
+        minor_matrix.append(minor_row)
+
+    return minor_matrix
