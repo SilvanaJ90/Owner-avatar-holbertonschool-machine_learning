804206083b3867fde1a6e814da7dfcf62d48a451
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-03-22 16:18:07 -0500

    create task multinormal

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -15,3 +15,15 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1).reshape(-1, 1)
         self.cov = np.dot(
             data - self.mean, (data - self.mean).T) / (data.shape[1] - 1)
+
+    def pdf(self, x):
+        """ Doc """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        if x.shape != (self.mean.shape[0], 1):
+            raise ValueError(f"x must have the shape ({self.mean.shape[0]}, 1)")
+        
+        d = self.mean.shape[0]
+        exponent = -0.5 * np.dot(np.dot((x - self.mean).T, np.linalg.inv(self.cov)), (x - self.mean))
+        coef = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(np.linalg.det(self.cov)))
+        return coef * np.exp(exponent)
