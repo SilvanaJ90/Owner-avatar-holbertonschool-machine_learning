1fb37b3644365ad5cf1e8c78b28c814cb72717ac
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-03-24 21:58:52 -0500

    create tsk 3

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -6,26 +6,24 @@ import numpy as np
 class MultiNormal:
     """ Doc """
     def __init__(self, data):
-        """Doc """
         if not isinstance(data, np.ndarray) or data.ndim != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
         if data.shape[1] < 2:
             raise ValueError("data must contain multiple data points")
-
-        self.mean = np.mean(data, axis=1).reshape(-1, 1)
-        self.cov = np.dot(
-            data - self.mean, (data - self.mean).T) / (data.shape[1] - 1)
-
+        
+        self.mean = np.mean(data, axis=1, keepdims=True)
+        centered_data = data - self.mean
+        self.cov = np.dot(centered_data, centered_data.T) / (data.shape[1] - 1)
+        
     def pdf(self, x):
         if not isinstance(x, np.ndarray):
             raise TypeError("x must be a numpy.ndarray")
         if x.ndim != 2 or x.shape != (self.mean.shape[0], 1):
             raise ValueError(f"x must have the shape ({self.mean.shape[0]}, 1)")
-
+        
         d = self.mean.shape[0]
         exponent = -0.5 * np.dot(np.dot((x - self.mean).T, np.linalg.inv(self.cov)), (x - self.mean))
         coefficient = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(np.linalg.det(self.cov)))
         pdf_value = coefficient * np.exp(exponent)
-
+        
         return pdf_value
-
