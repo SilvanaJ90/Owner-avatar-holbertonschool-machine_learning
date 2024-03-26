108318db5b7a1a6bcc4e9f5724fddafc4e4d7bef
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-03-26 17:08:54 -0500

    create task 0

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -3,6 +3,7 @@
  hypothetical probabilities of developing severe side effects:
 """
 import numpy as np
+from scipy.special import comb
 
 
 def likelihood(x, n, P):
@@ -29,7 +30,7 @@ def likelihood(x, n, P):
     """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x <= 0:
+    if not isinstance(x, int) or x < 0:
         raise ValueError(
             "x must be an integer that is greater than or equal to 0")
     if x > n:
@@ -38,9 +39,7 @@ def likelihood(x, n, P):
         raise TypeError("P must be a 1D numpy.ndarray")
     if (P < 0).any() or (P > 1).any():
         raise ValueError("All values in P must be in the range [0, 1]")
-
-    # Calculate likelihood for each probability in P
-    likelihoods = np.zeros_like(P)
-    for i, prob in enumerate(P):
-        likelihoods[i] = np.math.comb(n, x) * prob**x * (1 - prob)**(n - x)
-    return likelihoods
+    
+    cb = comb(n, x)
+    prob =  cb * P**x * (1 - P)**(n - x)
+    return prob
