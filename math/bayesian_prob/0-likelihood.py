07d39b014c5c59183ae69cb6f5e3df8a8bffb659
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-03-26 16:07:00 -0500

    create conditions

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -27,10 +27,11 @@ def likelihood(x, n, P):
     Returns: a 1D numpy.ndarray containing the likelihood of obtaining the data
     x and n, for each probability in P, respectively
     """
-    if not isinstance(n, int) or n  < 0:
-        raise ValueError ("n must be a positive integer")
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
