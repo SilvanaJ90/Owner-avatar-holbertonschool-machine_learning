08d43554cdf54c8fcba9a2463d7bb5b50268243d
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-03-27 09:19:47 -0500

    create task 2

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 """ calculates the marginal probability of obtaining the data:"""
-intersection = __import__('1-intersection').intersection
 import numpy as np
+
+intersection = __import__('1-intersection').intersection
 
 
 def marginal(x, n, P, Pr):
     """ Doc """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0: 
+    if not isinstance(x, int) or x < 0:
         raise ValueError(
             "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or P.ndim != 1: 
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError(
