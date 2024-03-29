acff6cfcaf6c2126747e7de3d077215318385342
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-03-28 20:43:50 -0500

    create task 1

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -4,25 +4,9 @@ import numpy as np
 
 
 def pca(X, var=0.95):
-    """ Doc """
-    # Calculate the covariance matrix
-    covariance_matrix = np.cov(X, rowvar=False)
-    
-    # Perform eigen decomposition
-    eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
-    
-    # Sort eigenvalues and corresponding eigenvectors
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    sorted_eigenvalues = eigenvalues[sorted_indices]
-    sorted_eigenvectors = eigenvectors[:, sorted_indices]
-    
-    # Calculate the cumulative explained variance
-    explained_variance = np.cumsum(sorted_eigenvalues) / np.sum(sorted_eigenvalues)
-    
-    # Determine the number of components to retain
-    nd = np.argmax(explained_variance >= var) + 1
-    
-    # Select the top nd eigenvectors
-    W = sorted_eigenvectors[:, :nd]
-    
-    return W
+    """ Calculates the PCA loadings """
+
+    _, s, vh = np.linalg.svd(X)
+    total_var = np.cumsum(s) / np.sum(s)
+    r = np.argwhere(total_var >= var)[0, 0]
+    return vh[:r + 1].T
