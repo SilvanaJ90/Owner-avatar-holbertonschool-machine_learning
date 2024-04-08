4c26dbffc10804161a31b3bfa486e819b63aaccd
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-04-08 05:27:50 -0500

    create task 1

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 """ Doc """
-
 import numpy as np
-initialize = __import__('0-initialize').initialize
+
+
+def initialize(X, k):
+    """
+    that initializes cluster centroids for K-means
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+
+    min_vals = np.min(X, axis=0)
+    max_vals = np.max(X, axis=0)
+    centroids = np.random.uniform(min_vals, max_vals, size=(k, X.shape[1]))
+
+    return centroids
 
 
 def kmeans(X, k, iterations=1000):
