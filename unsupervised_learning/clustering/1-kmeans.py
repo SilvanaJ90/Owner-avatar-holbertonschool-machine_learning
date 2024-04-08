1f60ac1491af0e5761e92859458c10ad37be0b4a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-04-08 05:42:49 -0500

    create task 1

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -2,10 +2,17 @@
 """ Doc """
 import numpy as np
 
-
 def initialize(X, k):
     """
-    that initializes cluster centroids K-means
+    Initialize cluster centroids  K-means.
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+        k: positive integer containing the number of clusters
+
+    Returns:
+        numpy.ndarray of shape (k, d) containing the initialized centroids each cluster,
+        or None on failure
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
@@ -18,7 +25,6 @@ def initialize(X, k):
 
     return centroids
 
-
 def kmeans(X, k, iterations=1000):
     """
     Performs K-means clustering on a dataset.
@@ -26,20 +32,21 @@ def kmeans(X, k, iterations=1000):
     Args:
         X: numpy.ndarray of shape (n, d) containing the dataset
         k: positive integer containing the number of clusters
-        iterations: positive integer
-        containing the maximum number of iterations
+        iterations: positive integer containing the maximum number of iterations
 
     Returns:
-        C: numpy.ndarray of shape (k, d) containing
-        the centroid means each cluster
-        clss: numpy.ndarray of shape (n,) containing the index
-        of the cluster each data point belongs to
+        C: numpy.ndarray of shape (k, d) containing the centroid means each cluster
+        clss: numpy.ndarray of shape (n,) containing the index of the cluster each data point belongs to
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None
+        return None, None
     if not isinstance(k, int) or k <= 0:
-        return None
+        return None, None
+
     n, d = X.shape
+
+    if n == 0:
+        return None, None
 
     # Initialize centroids
     C = initialize(X, k)
@@ -47,16 +54,11 @@ def kmeans(X, k, iterations=1000):
 
     for _ in range(iterations):
         # Assign each data point to the nearest centroid
-        distances = np.sqrt(((X[:, np.newaxis, :] - C)**2).sum(axis=2))
+        distances = np.sqrt(((X[:, np.newaxis, :] - C) ** 2).sum(axis=2))
         clss = np.argmin(distances, axis=1)
 
         # Update centroids
-        new_C = np.array(
-            [X[clss == i].mean(axis=0) if np.sum(
-                clss == i) > 0 else np.random.uniform(
-                    np.min(X, axis=0), np.max(
-                        X, axis=0)) for i in range(k)])
-
+        new_C = np.array([X[clss == i].mean(axis=0) if np.sum(clss == i) > 0 else np.random.uniform(np.min(X, axis=0), np.max(X, axis=0)) for i in range(k)])
 
         if np.array_equal(C, new_C):
             break
