74b8685ce4aabc62f68d2db0e0f4d2c343381257
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-09 17:25:51 -0500

    create task 10

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Performs K-means on a dataset: """
 import sklearn.cluster
-import numpy as np
+
 
 def kmeans(X, k):
     """
@@ -11,7 +11,8 @@ def kmeans(X, k):
     The only import you are allowed to use is import sklearn.cluster
     Returns: C, clss
         C is a numpy.ndarray of shape (k, d) containing the centroid means for each cluster
-        clss is a numpy.ndarray of shape (n,) containing the index of the cluster in C that each data point belongs to
+        clss is a numpy.ndarray of shape (n,) containing the index of the cluster in
+         C that each data point belongs to
 
 
     """
