da35f9f26ca2d8323a8030af885c58da56ed7e3e
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-09 17:33:01 -0500

    create task 10

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Performs K-means on a dataset: """
-import numpy as np
 import sklearn.cluster
 
 
 def kmeans(X, k):
+    """ Doc """
     kmeans_model = sklearn.cluster.KMeans(n_clusters=k)
     kmeans_model.fit(X)
     C = kmeans_model.cluster_centers_
