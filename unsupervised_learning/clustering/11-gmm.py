4a1e78daf2e928a8fd1b06e6240fa945268496c7
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-09 21:21:48 -0500

    create task 11

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """ that calculates a GMM from a dataset """
 
-import numpy as np
-from sklearn.mixture import GaussianMixture
 
+import sklearn.mixture
 
 def gmm(X, k):
     """ that calculates a GMM from a dataset """
     # Inicializar el modelo GMM
-    gmm_model = GaussianMixture(n_components=k)
+    gmm_model = sklearn.mixture.GaussianMixture(n_components=k)
 
     # Ajustar el modelo a los datos
     gmm_model.fit(X)
@@ -25,8 +24,8 @@ def gmm(X, k):
     kmin, kmax = 1, 10  # Cambia estos valores según sea necesario
     bic = []
     for k_test in range(kmin, kmax + 1):
-        gmm_model_test = GaussianMixture(n_components=k_test)
+        gmm_model_test = sklearn.mixture.GaussianMixture(n_components=k_test)
         gmm_model_test.fit(X)
         bic.append(gmm_model_test.bic(X))
 
-    return pi, m, S, clss, np.array(bic)
+    return pi, m, S, clss, (bic)
