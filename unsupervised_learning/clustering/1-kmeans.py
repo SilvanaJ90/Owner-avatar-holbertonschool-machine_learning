9402d507a7c635b6199f794832230038355214bf
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-10 23:00:42 -0500

    create task 1

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,48 +1,22 @@
 #!/usr/bin/env python3
+""" Doc """
 import numpy as np
 
 
-def initialize(X, k):
-    """
-    Initializes cluster centroids for K-means clustering.
-
-    Args:
-        X: numpy.ndarray of shape (n, d) containing the dataset for clustering
-        k: positive integer, number of clusters
-
-    Returns:
-        numpy.ndarray of shape (k, d) containing the initialized centroids for each cluster,
-        or None on failure
-    """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2 or not isinstance(k, int) or k <= 0:
+def kmeans(X, k, iterations=1000):
+    """ Doc """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+    n, d = X.shape
 
+    # Inicializar los centroides de los clusters
     min_vals = np.min(X, axis=0)
     max_vals = np.max(X, axis=0)
-    centroids = np.random.uniform(low=min_vals, high=max_vals, size=(k, X.shape[1]))
 
-    return centroids
+    centroids = np.random.uniform(min_vals, max_vals, size=(k, d))
 
-
-def kmeans(X, k, iterations=1000):
-    """
-    Performs K-means clustering on a dataset.
-
-    Args:
-        X: numpy.ndarray of shape (n, d) containing the dataset
-        k: positive integer, number of clusters
-        iterations: positive integer, maximum number of iterations
-
-    Returns:
-        C: numpy.ndarray of shape (k, d) containing the centroid means for each cluster
-        clss: numpy.ndarray of shape (n,) containing the index of the cluster in C that each data point belongs to
-    """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None
-    if not isinstance(k, int) or k <= 0:
-        return None, None
-
-    centroids = initialize(X, k)
     if centroids is None:
         return None, None
 
