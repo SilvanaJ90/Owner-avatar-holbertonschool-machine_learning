fb6e3fbc472f7c624469524a42b784357ed107d8
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-10 22:43:33 -0500

    create task 1

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -37,6 +37,11 @@ def kmeans(X, k, iterations=1000):
         C: numpy.ndarray of shape (k, d) containing the centroid means for each cluster
         clss: numpy.ndarray of shape (n,) containing the index of the cluster in C that each data point belongs to
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None
+
     centroids = initialize(X, k)
     if centroids is None:
         return None, None
