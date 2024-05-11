a7ac6ff55cccddf2469477e6f635d744108c3dfa
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-10 23:23:03 -0500

    create task 1

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -20,16 +20,23 @@ def kmeans(X, k, iterations=1000):
     if centroids is None:
         return None, None
 
-    for _ in range(iterations):
+    for i in range(iterations):
+        # Asignación de Puntos a los Centroides más Cercanos: dist(p,q)=i=1∑d​(pi​−qi​)2
         distances = np.linalg.norm(X[:, np.newaxis] - centroids, axis=2)
         clss = np.argmin(distances, axis=1)
 
+        C = np.array([X[clss == c].mean(axis=0) for c in range(k)])
+
         C = np.array([X[clss == c].mean(axis=0) if np.sum(clss == c) > 0 else np.random.uniform(
             np.min(X, axis=0), np.max(X, axis=0)) for c in range(k)])
+        
+        for c in range(k):
+            if X[clss == c].size == 0:
+                C[c] = np.random.uniform(min_vals, max_vals, size=(1, d))
+
 
         if np.array_equal(centroids, C):
             break
 
-        centroids = C
 
     return centroids, clss
