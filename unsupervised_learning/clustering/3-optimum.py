d032e30d1ce5d84ad4a1f15e2dc6f020f632cac5
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-10 22:16:03 -0500

    create task 3

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,31 +1,14 @@
 #!/usr/bin/env python3
 """ Tests for the optimum number of clusters by variance: """
 
+import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 variance = __import__('2-variance').variance
-import numpy as np
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
-
-    X is a numpy.ndarray of shape (n, d) containing the data set
-    kmin is a positive integer containing the minimum number
-      of clusters to check for (inclusive)
-    kmax is a positive integer containing the maximum number
-      of clusters to check for (inclusive)
-    iterations is a positive integer containing the
-    maximum number of iterations for K-means
-    This function should analyze at least 2 different cluster sizes
-    You should use:
-        kmeans = __import__('1-kmeans').kmeans
-        variance = __import__('2-variance').variance
-    You may use at most 2 loops
-    Returns: results, d_vars, or None, None on failure
-        results is a list containing the outputs of K-means
-          for each cluster size
-        d_vars is a list containing the difference in variance
-        from the smallest cluster size for each cluster size
+    Doc
 
     """
     if type(X) is not np.ndarray or X.ndim != 2:
