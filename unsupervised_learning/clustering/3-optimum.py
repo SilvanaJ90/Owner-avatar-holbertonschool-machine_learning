edd93f8aadff08f588a5ca964453b7addab24080
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-10 22:13:35 -0500

    create task 3

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Tests for the optimum number of clusters by variance: """
+
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 variance = __import__('2-variance').variance
