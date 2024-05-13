37b43073b4dbf2a31782bd6f4d97877c243d6882
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-13 15:13:26 -0500

    create task 0

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -14,7 +14,7 @@ class GaussianProcess:
         self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
-        """Calculates the covariance kernel matrix between two matrices"""
+        """ Calculates the covariance kernel matrix between two matrices """
         sqdist = np.sum(X1**2, 1).reshape(-1, 1) \
             + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
