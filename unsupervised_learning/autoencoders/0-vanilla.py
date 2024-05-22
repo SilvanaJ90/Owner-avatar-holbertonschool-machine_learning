61b941f600bb169bd137457ef2778f89886eb237
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-21 20:37:55 -0500

    create task 0

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """ creates an autoencoder: """
-
-import tensorflow as tf
-from tensorflow.keras import layers, models
+import tensorflow.keras as keras
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
