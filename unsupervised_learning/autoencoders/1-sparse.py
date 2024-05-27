97834281f0928f346bc76f1f43e166dc509463cc
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-26 19:39:58 -0500

    create task 1

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -26,33 +26,37 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     """
     input_layer = keras.layers.Input(shape=(input_dims,))
     encoded = input_layer
-    
+
     for nodes in hidden_layers:
         encoded = keras.layers.Dense(nodes, activation='relu')(encoded)
-    
-    latent_layer = keras.layers.Dense(latent_dims, activation='relu', activity_regularizer=keras.regularizers.L1(lambtha))(encoded)
-    
+
+    latent_layer = keras.layers.Dense(
+        latent_dims,
+        activation='relu',
+        activity_regularizer=keras.regularizers.L1(lambtha))(encoded)
+
     encoder = keras.models.Model(input_layer, latent_layer)
-    
+
     # Decoder
     decoded_input = keras.layers.Input(shape=(latent_dims,))
     decoded = decoded_input
-    
+
     for nodes in reversed(hidden_layers):
         decoded = keras.layers.Dense(nodes, activation='relu')(decoded)
-    
-    output_layer = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
-    
+
+    output_layer = keras.layers.Dense(
+        input_dims, activation='sigmoid')(decoded)
+
     decoder = keras.models.Model(decoded_input, output_layer)
-    
+
     # Autoencoder
     auto_input = input_layer
     encoded_output = encoder(auto_input)
     decoded_output = decoder(encoded_output)
-    
+
     auto = keras.models.Model(auto_input, decoded_output)
-    
+
     # Compile the autoencoder model
     auto.compile(optimizer='adam', loss='binary_crossentropy')
-    
+
     return encoder, decoder, auto
