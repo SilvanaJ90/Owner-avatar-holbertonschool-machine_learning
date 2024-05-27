e7abf34f688b33f51a72db2e98c59c21b12409fc
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-26 19:35:58 -0500

    create task 1

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """ creates a sparse autoencoder """
+import tensorflow as tf
+from tensorflow.keras import layers, models, regularizers
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
@@ -23,3 +25,35 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     layer in the decoder, which should use sigmoid
 
     """
+    input_layer = layers.Input(shape=(input_dims,))
+    encoded = input_layer
+    
+    for nodes in hidden_layers:
+        encoded = layers.Dense(nodes, activation='relu')(encoded)
+    
+    latent_layer = layers.Dense(latent_dims, activation='relu', activity_regularizer=regularizers.L1(lambtha))(encoded)
+    
+    encoder = models.Model(input_layer, latent_layer)
+    
+    # Decoder
+    decoded_input = layers.Input(shape=(latent_dims,))
+    decoded = decoded_input
+    
+    for nodes in reversed(hidden_layers):
+        decoded = layers.Dense(nodes, activation='relu')(decoded)
+    
+    output_layer = layers.Dense(input_dims, activation='sigmoid')(decoded)
+    
+    decoder = models.Model(decoded_input, output_layer)
+    
+    # Autoencoder
+    auto_input = input_layer
+    encoded_output = encoder(auto_input)
+    decoded_output = decoder(encoded_output)
+    
+    auto = models.Model(auto_input, decoded_output)
+    
+    # Compile the autoencoder model
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+    
+    return encoder, decoder, auto
