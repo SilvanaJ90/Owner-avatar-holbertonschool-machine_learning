91a5907a222d6b0797c43b2ea6ee647491129251
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-05-28 17:34:13 -0500

    update

--- supervised_learning/RNNs/2-gru_cell.py
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -6,6 +6,24 @@ class GRUCell:
     """represents a gated recurrent unit """
 
     def __init__(self, i, h, o):
+        """
+
+        i is the dimensionality of the data
+        h is the dimensionality of the hidden state
+        o is the dimensionality of the outputs
+        Creates the public instance attributes Wz, Wr, Wh, Wy, bz, br, bh, by
+        that represent the weights and biases of the cell
+            Wzand bz are for the update gate
+            Wrand br are for the reset gate
+            Whand bh are for the intermediate hidden state
+            Wyand by are for the output
+        The weights should be initialized using a random normal
+        distribution in the order listed above
+        The weights will be used on the right side for matrix multiplication
+        The biases should be initialized as zeros
+
+        """
+
         # Initialize weights and biases for the update gate
         self.Wz = np.random.randn(i + h, h)
         self.bz = np.zeros((1, h))
@@ -23,6 +41,17 @@ class GRUCell:
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
+        """
+        x_t is a numpy.ndarray of shape (m, i)
+        that contains the data input for the cell
+            m is the batche size for the data
+        h_prev is a numpy.ndarray of shape (m, h)
+        containing the previous hidden state
+        The output of the cell should use a softmax activation function
+        Returns: h_next, y
+            h_next is the next hidden state
+            y is the output of the cell
+        """
         # Concatenate the input and the previous hidden state
         h_x = np.concatenate((h_prev, x_t), axis=1)
 
